3c55827e93756075c01a4e22b118bc0f43db34f9
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-01-04 21:28:44 +0200

    [bug fix /libft] fix incorect argument passed to write

--- libft/ft_putstr.c
+++ libft/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/27 16:01:59 by ikoloshy          #+#    #+#             */
-/*   Updated: 2018/12/27 19:31:02 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/01/04 20:36:32 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ int	ft_putstr(char const *s)
 	if (!s)
 		return (-1);
 	len = (int)ft_strlen(s);
-	write(1, &s, len);
+	write(1, s, len);
 	return (len);
 }
