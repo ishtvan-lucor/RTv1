36a6c5665fb66ab3815e0a5ff92b644f15c97c86
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-01-05 21:40:57 +0200

    [add /src] wrote logic of finding obj. NEED to add logic about throwing obj to validation

---
+++ src/val_sphere.c
@@ -0,0 +1,4 @@
+//
+// Created by Ishtvan KOLOSHYNSKY on 05.01.2019.
+//
+
--- src/validation_config.c
+++ src/validation_config.c
@@ -6,11 +6,58 @@
 /*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 19:58:34 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/01/03 21:21:34 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/01/05 21:33:25 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/RTv1.h"
+
+// TODO write logic for create/add list with obj(check obj before adding to list OR NO!)
+
+static int	check_light(int fd, int obj, t_list *light)
+{
+
+}
+
+static int	check_prim(int fd, int obj, t_list *prim)
+{
+
+
+}
+
+//TODO add new primitives and lights
+
+static int	define_obj(char *line)
+{
+	if (!ft_strcmp(line, "sphere"))
+		return (SPHERE);
+	else if (!ft_strcmp(line, "spot"))
+		return (SPOT);
+	else
+		return (-1);
+}
+
+static int	find_objects(int fd, t_list *light, t_list *prim)
+{
+	char	*line;
+	int		obj;
+	int		error;
+
+	while (get_next_line(fd, &line) == 1)
+	{
+		obj = define_obj(line);
+		if (obj == -1)
+			error = ft_putstr("ERROR: invalid object name! MAN_CONF");
+		else if (obj < 10)
+			error = check_light(fd, obj, light);
+		else
+			error = check_prim(fd, obj, prim);
+		free(line);
+		if (error)
+			return (1);
+	}
+	return (0);
+}
 
 int			validation_config(char *config, t_basic *sv)
 {
@@ -20,6 +67,8 @@ int			validation_config(char *config, t_basic *sv)
 		return (ft_putstr("ERROR: fail read!\n"));
 	if (val_camera(fd, &sv->cmr))
 		return (ft_putstr("ERROR: incorrect camera data! MAN_CONF\n"));
+	if (find_objects(fd, sv->light, sv->prim))
+		return (1);
 	if (close(fd) == -1)
 		return (ft_putstr("ERROR: fail close!\n"));
 	return (0);
