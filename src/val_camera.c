01eb7200857e3609d0d8a49f832354862bc58707
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-01-05 18:34:58 +0200

    [mod src/] ended validation camera point

--- src/val_camera.c
+++ src/val_camera.c
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 20:22:32 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/01/03 21:24:02 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/01/05 17:26:12 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ static int	get_points(const char *line, t_vector *cmr)
 	size_t	len;
 	int		error_f;
 
-	error_f = 0;
 	if (!(s = ft_strsplit(line, ' ')))
 		return (ft_putstr("ERROR: to big string=)\n"));
 	if ((len = ft_wordssplit(line, ' ')) != 4)
-		ft_putstr("ERROR: useless information is present 0_0\n");
-	else if (ft_strcmp(s[1], "camera"))
-		error_f = ft_putstr("ERROR: first line should be camera's position)\n");
-	if (error_f != 4)
 	{
 		ft_free_after_split(s, len);
-		return (error_f);
+		return (ft_putstr("ERROR: useless information is present 0_0\n"));
 	}
-	error_f = vector_writer(s[2], s[3], s[4], cmr);
+	if (ft_strcmp(s[0], "camera"))
+	{
+		ft_free_after_split(s, len);
+		return (ft_putstr("ERROR: first line should be camera's position)\n"));
+	}
+	error_f = vector_writer(s[1], s[2], s[3], cmr);
 	ft_free_after_split(s, len);
 	return (error_f);
 }
