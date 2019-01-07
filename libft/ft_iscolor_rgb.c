5f843da3dae51472b163cb96bffa7b55ecf79cb0
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-01-07 15:38:47 +0200

    [refactor /libft] invert returned value

--- libft/ft_iscolor_rgb.c
+++ libft/ft_iscolor_rgb.c
@@ -18,22 +18,22 @@ int	ft_iscolor_rgb(const char *s)
 
 	len = 6;
 	if (!s)
-		return (0);
+		return (1);
 	if (*s == '0')
 		s++;
 	if (*s == 'x' || *s == 'X')
 		s++;
 	if (!(*s))
-		return (0);
+		return (1);
 	while (*s && len-- > 0)
 	{
 		if (ft_isdigit((int)(*s)) || (*s >= 'A' && *s <= 'F') ||
 				(*s >= 'a' && *s <= 'f'))
 			s++;
 		else
-			return (0);
+			return (1);
 	}
 	if (*s)
-		return (0);
-	return (1);
+		return (1);
+	return (0);
 }
---
+++ src/val_data_obj_1.c
@@ -0,0 +1,4 @@
+//
+// Created by Ishtvan KOLOSHYNSKY on 1/7/19.
+//
+
