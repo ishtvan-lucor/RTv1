38c80d8639f6a726f509d0150e9b4aef1b6ad98f
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-01-13 18:32:25 +0200

    [added /libft] added main operation for RTv1 on vectors exepted basic

--- libft/ft_operation_on_vector.c
+++ libft/ft_operation_on_vector.c
@@ -6,25 +6,33 @@
 /*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 21:19:31 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/01/10 21:32:41 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/01/13 18:04:27 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-double	ft_len_vector(const t_vector vec)
+double	v_prd(t_vector v1, t_vector v2)
 {
-	return (sqrt(vec.x *vec.x + vec.y * vec.y + vec.z * vec.z));
+	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
 }
 
-double	ft_dot_vector(const t_vector vec)
+double	v_len(t_vector vec)
+{
+	return (sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z));
+}
+
+double	v_dot(t_vector vec)
 {
 	double	len_vec;
 
-	len_vec = ft_len_vector(vec);
-	return (vec * vec);
+	len_vec = v_len(vec);
+	return (len_vec * len_vec);
 }
 
+double v_nrm(t_vector v)
+{
+	return (v/(sqrt(v.x *v.x + v.y * v.y + v.z * v.z)));
+}
 
-// TODO write another useful vec operation like nrm, prodact -- check else
 // TODO check that they work properly
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 17:27:31 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/01/10 21:25:39 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/01/13 18:03:49 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,9 @@ size_t				ft_wordssplit(char const *s, char c);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 double				ft_string_to_double(const char *str);
-double			ft_len_vector(const t_vector vec);
-double			ft_dot_vector(const t_vector vec);
+double				v_prd(t_vector v1, t_vector v2);
+double				v_len(t_vector vec);
+double				v_dot(t_vector vec);
+double				v_nrm(t_vector v);
 
 #endif
