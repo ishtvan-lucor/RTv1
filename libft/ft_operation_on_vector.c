d390a6a2cecb034ca49123b9fe4d8ca10bd0ee1d
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-02-10 17:11:25 +0200

    refactor: delete func v_dot() and renamed func v_prd() on v_dot_prd() because v_dot() is the same like v_dot_prd() but only for one vector

--- libft/ft_operation_on_vector.c
+++ libft/ft_operation_on_vector.c
@@ -6,13 +6,13 @@
 /*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 21:19:31 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/02/07 20:42:28 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/02/10 17:04:49 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-double		v_prd(const t_vector *v1, const t_vector *v2)
+double		v_dot_prd(const t_vector *v1, const t_vector *v2)
 {
 	return (v1->x * v2->x + v1->y * v2->y + v1->z * v2->z);
 }
@@ -20,14 +20,6 @@ double		v_prd(const t_vector *v1, const t_vector *v2)
 double		v_len(const t_vector *vec)
 {
 	return (sqrt(vec->x * vec->x + vec->y * vec->y + vec->z * vec->z));
-}
-
-double		v_dot(const t_vector *vec)
-{
-	double	len_vec;
-
-	len_vec = v_len(vec);
-	return (len_vec * len_vec);
 }
 
 t_vector	*v_nrm(const t_vector *v)
@@ -40,6 +32,3 @@ t_vector	*v_nrm(const t_vector *v)
 	*nrm = v_div_n(v, len);
 	return (nrm);
 }
-
-
-//TODO check on right work of vector operation func
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 17:27:31 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/02/07 20:38:14 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/02/10 17:04:20 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,9 +110,8 @@ size_t				ft_wordssplit(char const *s, char c);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 double				ft_string_to_double(const char *str);
-double				v_prd(const t_vector *v1, const t_vector *v2);
+double				v_dot_prd(const t_vector *v1, const t_vector *v2);
 double				v_len(const t_vector *vec);
-double				v_dot(const t_vector *vec);
 t_vector			*v_nrm(const t_vector *v);
 t_vector			v_plus(const t_vector *v1, const t_vector *v2);
 t_vector			v_minus(const t_vector *v1, const t_vector *v2);
