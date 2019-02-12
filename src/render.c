f02ce030687e66481819b04e69483fded2cca813
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-02-12 22:15:10 +0200

    refactor/feat: finished render func + start trace_ray(stopping due of rafactoring struct is need) remove func point_from_screen_to_viewport from own funct to rendre

--- src/render.c
+++ src/render.c
@@ -6,34 +6,45 @@
 /*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 21:22:10 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/01/10 21:09:34 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/02/12 17:09:15 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/RTv1.h"
+#include "rtv1.h"
 
-static int	get_color(t_vector d, t_vector cmr, t_list *obj, t_list *l)
+static	t_vector	point_from_screen_to_viewport(int x, int y)
 {
+	t_vector	temp;
 
+	temp.x = FOV * ((double)x / WIN_W - 0.5);
+	temp.y = FOV * (0.5 - (double)y / WIN_H);
+	temp.z = FOV;
+	return (temp);
 }
 
-void		render(t_basic *s)
+void				render(t_basic *s)
 {
 	int			x;
 	int			y;
-	t_vector		d;
+	t_data_tr	dtr;
 
 	x = 0;
 	y = 0;
+
 	while (y < WIN_H)
 	{
-		while(x < WIN_H)
+		while (x < WIN_H)
 		{
-			d = point_from_screen_to_viewport(x, y);
-			s->img[x + y * WIN_W] = get_color(d, s->cmr, s->prim, s->light);
+			dtr.min = START_OF_VIEWPORT;
+			dtr.max = INFINITY_RAY_DIST;
+			dtr.d = point_from_screen_to_viewport(x, y);
+			dtr.o = s->cmr;
+			s->img[x + y * WIN_W] = trace_ray(&dtr, s->prim, s->light, DEPTH);
 			x++;
 		}
 		y++;
 	}
 }
-// TODO write render universal there. render of sphere in other file
+
+// TODO check correctly work point_from_screen_to_viewport
+//TODO add funct that make right angle for camera view
--- src/trace_ray.c
+++ src/trace_ray.c
@@ -1,4 +1,26 @@
-//
-// Created by Ishtvan KOLOSHYNSKY on 2019-02-12.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trace_ray.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/12 16:37:37 by ikoloshy          #+#    #+#             */
+/*   Updated: 2019/02/12 21:16:31 by ikoloshy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "rtv1.h"
+
+int				trace_ray(t_data_tr *d, const t_list *obj, const t_list *light)
+{
+	t_co	clst_obj;
+
+	clst_obj.obj = NULL;
+	get_closest_object(&clst_obj, d);
+	if (clst_obj.obj == NULL)
+		return (BACKGROUND_COLOR);
+
+}
+
+// TODO after making universal struct
