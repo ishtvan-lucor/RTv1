d7c7315a4d120150ccdc1aa3435e0a8e461fe27a
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-02-21 17:29:34 +0200

    feat: remove func vector_writer in file val_data_for_obj.c

--- src/val_data_for_obj.c
+++ src/val_data_for_obj.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   val_data_obj_1.c                                   :+:      :+:    :+:   */
+/*   val_data_for_obj.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikoloshy <ikoloshy@student.unit.ua>        +#+  +:+       +#+        */
+/*   By: ikoloshy <ikoloshy@unit.student.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 15:09:50 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/01/07 20:27:19 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/02/21 17:25:46 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,9 @@ int	val_color(char **s, t_color *trg, size_t len)
 
 int	val_double_cord(char **s, double *trg, size_t len)
 {
-	int	type;
-
 	if (len != 2)
 		return (ft_putstr("ERROR:{dbl} invalid amount of data!\n"));
-	if (!(type = ft_isdouble_or_int(s[1])))
+	if (!ft_isdouble_or_int(s[1]))
 		return (ft_putstr("ERROR: invalid value!\n"));
 	*trg = ft_string_to_double(s[1]);
 	return (0);
@@ -69,4 +67,22 @@ int	val_vector(char **s, t_vector *trg, size_t len)
 		return (ft_putstr("ERROR:{vctr} invalid amount of data!\n"));
 	error_code = vector_writer(s[1], s[2], s[3], trg);
 	return (error_code);
+}
+
+/*
+**  Val every coordinate and than save them
+*/
+
+int	vector_writer(char *c1, char *c2, char *c3, t_vector *t)
+{
+	if (!c1 || !ft_isdouble_or_int(c1))
+		return (ft_putstr("ERROR: first coordinate invalid!\n"));
+	if (!c2 || !ft_isdouble_or_int(c2))
+		return (ft_putstr("ERROR: second coordinate invalid!\n"));
+	if (!c3 || !ft_isdouble_or_int(c3))
+		return (ft_putstr("ERROR: third coordinate invalid!\n"));
+	t->x = ft_string_to_double(c1);
+	t->y = ft_string_to_double(c2);
+	t->z = ft_string_to_double(c3);
+	return (0);
 }
