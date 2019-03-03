13ac61c6d7923c3f435fe371b28ab054fffaef3e
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-03-03 21:35:33 +0200

    feat: add discriminant for s_values, type for s_closet_object, enum for simple understanding intersection calculation, defines for validation

--- includes/usable_structs.h
+++ includes/usable_structs.h
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <ikoloshy@unit.student.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 17:05:07 by ikoloshy          #+#    #+#             */
-/*   Updated: 2019/02/17 15:00:10 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2019/03/03 18:08:39 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,12 @@
 # define SPC "specular"
 # define INTN "intensity"
 # define PSTN "position"
+# define NRM "normal"
+# define POINT "point"
+# define AXIS "axis"
+# define ANGLE "angle"
+# define ROT_X "rotationX"
+# define ROT_Y "rotationY"
 # define DELIMITR "+"
 
 typedef enum	e_rgb
@@ -33,17 +39,24 @@ typedef enum	e_rgb
 	BLUE
 }				t_rgb;
 
+typedef enum	e_val_intersection
+{
+	DOT_DV,
+	DOT_XV,
+	KK1
+}				t_vi;
+
 typedef union	u_color
 {
-	int 		color;
+	int			color;
 	UC			clr[3];
 }				t_color;
 
 typedef struct	s_image_specification
 {
-	int 		bpp;
-	int 		sl;
-	int 		endl;
+	int			bpp;
+	int			sl;
+	int			endl;
 }				t_is;
 
 typedef struct	s_data_tr
@@ -57,6 +70,7 @@ typedef struct	s_data_tr
 typedef struct	s_closet_object
 {
 	void		*obj;
+	size_t		type;
 	int			color;
 	int			spcl;
 	double		rfl;
@@ -77,6 +91,7 @@ typedef struct	s_values
 	double		a;
 	double		b;
 	double		c;
+	double		dscr;
 }				t_values;
 
 #endif
